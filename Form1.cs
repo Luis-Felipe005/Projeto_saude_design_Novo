fc4d7aaf4fd9049fa2bdcd6ee0cbdeb3adc86c34
Luis-Felipe005 <135838158+Luis-Felipe005@users.noreply.github.com>
2023-09-12 14:35:54 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Tela_teste_login2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            var strConexao = "server=localhost;uid=luis;pwd=3412;database=cadastro";
+            var conexao = new MySqlConnection(strConexao);
+    
+            conexao.Open();
+        }
+
+
+        MySqlConnection conexao1 = new MySqlConnection(Properties.Settings.Default.conexao);
+
+        string usuario, senha;
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            Logar();
+        }
+
+        private void Logar()
+        {
+
+            string login = "SELECT CPF, Senha FROM pacientes WHERE CPF='"+txtLogin.Text+"' AND Senha='"+txtSenha.Text+"'";
+            MySqlCommand comando = new MySqlCommand(login, conexao1);
+            comando.CommandType = CommandType.Text;
+            MySqlDataReader reader;
+
+            try
+            {
+
+                conexao1.Open();
+                reader = comando.ExecuteReader();
+                if(reader.Read())
+                {
+
+                    usuario = reader[0].ToString();
+                    senha = reader[1].ToString();
+
+                    conexao1.Close();
+                    
+                    if(usuario==txtLogin.Text && senha == txtSenha.Text) 
+                    { 
+                    
+                   Form form = new Form();
+                        form.Show();
+                        this.Hide();
+                    
+                    }
+
+
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Deu erro" + ex);
+
+
+
+            }
+
+           
+        }
+
+
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Tela_teste_login2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewAtendente = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listViewAtendente
+            // 
+            this.listViewAtendente.HideSelection = false;
+            this.listViewAtendente.Location = new System.Drawing.Point(85, 50);
+            this.listViewAtendente.Name = "listViewAtendente";
+            this.listViewAtendente.Size = new System.Drawing.Size(246, 166);
+            this.listViewAtendente.TabIndex = 0;
+            this.listViewAtendente.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listViewAtendente);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewAtendente;
+    }
+}
